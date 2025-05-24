b60de2dc9adf8e5aa4d0346895567ffae8634e14
aznoH333 <glentik23@gmail.com>
2025-05-24 12:02:01 +0200

    initial commit

---
+++ MIPY_SIFRY/src/cypher/cypher_util.py
@@ -0,0 +1,25 @@
+class CypherUtil:
+    
+    
+    DEFAULT_KEY = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"]
+    
+    def __init__ (self, key):
+        self.setKey(key)
+    
+    def setKey(self, newKey):
+        self.key = list(newKey.upper())
+
+
+    def encrypt(self, text):
+        out = ""
+        for char in text.upper():
+            out += self.DEFAULT_KEY[self.key.index(char)]
+        
+        return out
+
+    def decrypt(self, text):
+        out = ""
+        for char in text.upper():
+            out += self.key[self.DEFAULT_KEY.index(char)]
+        
+        return out
---
+++ MIPY_SIFRY/src/main.py
@@ -0,0 +1,13 @@
+from cypher.cypher_util import CypherUtil
+
+
+print("hello world")
+
+a = CypherUtil("VLZODTQHUXWSERMCFKNYIBJGP_A");
+print(a.key);
+print(a.DEFAULT_KEY);
+
+b = a.encrypt("TEST_TEST")
+
+print(b)
+print(a.decrypt(b))
