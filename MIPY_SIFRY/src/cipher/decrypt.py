87b978df539e88543196c5774834bb02c3bf6973
aznoH333 <glentik23@gmail.com>
2025-06-01 17:50:53 +0200

    raised iteration count

---
+++ MIPY_SIFRY/src/cipher/decrypt.py
@@ -0,0 +1,39 @@
+from cipher.bygram_matrix import plausability
+from cipher.cipher_util import decrypt, generate_random_key, sanitize_text, shuffle_key
+import random
+
+
+RANDOM_ACCEPT_CHANCE = 0.01
+
+def break_encryption(text, probability_matrix, attempts):
+    text = sanitize_text(text)
+    
+    key = generate_random_key()
+
+    best_key = key.copy()
+
+    
+    best_score = plausability(decrypt(key, text), probability_matrix)
+
+
+    for iteration in range(0, attempts):
+        
+        # iterate key
+        new_key = shuffle_key(key.copy())
+        
+        decrypted = decrypt(new_key, text)
+
+        score = plausability(decrypted, probability_matrix)
+
+        if score > best_score:
+            best_key = key.copy()
+            best_score = score
+            key = new_key
+
+        if random.uniform(0.0, 1.0) < RANDOM_ACCEPT_CHANCE:
+            key = new_key
+
+
+    return best_key
+
+
